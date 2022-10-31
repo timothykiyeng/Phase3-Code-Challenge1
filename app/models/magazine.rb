5f1f3d8a870379c594a75c2be9c905ab3ced539e
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-10-31 07:14:41 +0300

    Added contributors instance and then filtered from articles and then mapped each article to return a unique author

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -8,6 +8,14 @@ class Magazine
     ##shovel each new instance into an array
     @@all << self
   end
+     ##helper function
+  def magazine_articles
+    Article.all.select{|article| article.magazine.name == self.name}
+  end
+
+  def contributors
+    magazine_articles.collect{|article| article.author}.uniq
+  end
 
 
 end
